0741d8e807cca7cab6af3f808895687c53e80332
Mustafa Tanrıverdi <thetanriverdi@gmail.com>
2017-04-12 14:09:44 +0300

    config delete

--- til-config.php
+++ til-config.php
@@ -2,6 +2,6 @@
 define('_serverName', 'localhost'); 
 define('_userName', 'root'); 
 define('_userPassword', ''); 
-define('_dbName', 'bosdatabase'); 
+define('_dbName', 'tilpark'); 
 define('_prefix', 'til_'); 
-define('_site_url', 'http://192.168.2.149/github/tilpark');
+define('_site_url', 'http://192.168.2.254/github/tilpark');
