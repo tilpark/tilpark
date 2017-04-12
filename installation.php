b5aa2a9b6cba7131e42a790c6abd7cd1c7297b4d
Mustafa Tanrıverdi <thetanriverdi@gmail.com>
2017-04-12 14:46:07 +0300

    installaiton

--- installation.php
+++ installation.php
@@ -7,6 +7,8 @@
 		return $til;
 	}
 
+	@file_get_contents('http://api.tilpark.org/log.php?type=installation&step=0&url='. str_replace('installation.php', '', $_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']) .'&user_ip='.$_SERVER['SERVER_ADDR']);
+
 	include ('includes/input.php');
 
 	// DATEBASE CONTROL
